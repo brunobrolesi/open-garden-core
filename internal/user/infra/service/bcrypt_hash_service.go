5955d59b29e78958e1d63aabf7ec40bdd82da03c
Bruno Brolesi <bruno.brolesi@mercadolivre.com>
2022-10-30 17:30:02 -0300

    commit
    feat: add bcrypt hash service

---
+++ internal/user/infra/service/bcrypt_hash_service.go
@@ -0,0 +1,27 @@
+package service
+
+import (
+	"github.com/brunobrolesi/open-garden-core/internal/user/business/gateway"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type bcryptHashService struct {
+	HashCost int
+}
+
+func NewBcryptHashService(hashCoast int) gateway.HashService {
+	return bcryptHashService{
+		HashCost: hashCoast,
+	}
+}
+
+func (b bcryptHashService) GenerateHash(s string) (gateway.Hash, error) {
+	bytes := []byte(s)
+	hash, err := bcrypt.GenerateFromPassword(bytes, b.HashCost)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(hash), nil
+}
