91e643f8302ef7d8be40eaf3043242b6104febac
Bruno Brolesi <bruno.brolesi@mercadolivre.com>
2022-11-17 18:09:18 -0300

    feat: add get user by email in postgres repository

--- internal/user/infra/repository/postgres_user_repository.go
+++ internal/user/infra/repository/postgres_user_repository.go
@@ -9,7 +9,8 @@ import (
 )
 
 const (
-	createUserQuery = "insert into users(company_name, email, password, active) values($1, $2, $3, $4) returning (id)"
+	createUserQuery     = "insert into users(company_name, email, password, active) values($1, $2, $3, $4) returning (id)"
+	getUserByEmailQuery = "select id, company_name, email, password, active from users where email=$1"
 )
 
 type postgresUserRepository struct {
@@ -24,5 +25,11 @@ func NewPostgresUserRepository(conn *pgx.Conn) gateway.UserRepository {
 
 func (r postgresUserRepository) CreateUser(user model.User, ctx context.Context) (model.User, error) {
 	err := r.conn.QueryRow(ctx, createUserQuery, user.CompanyName, user.Email, user.Password, user.Active).Scan(&user.Id)
+	return user, err
+}
+
+func (r postgresUserRepository) GetUserByEmail(email string, ctx context.Context) (model.User, error) {
+	user := model.User{}
+	err := r.conn.QueryRow(ctx, getUserByEmailQuery, email).Scan(&user.Id, &user.CompanyName, &user.Email, &user.Password, &user.Active)
 	return user, err
 }
