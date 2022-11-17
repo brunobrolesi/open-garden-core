0de69001a65b3ee076c7309fb86abff6b6de2249
Bruno Brolesi <bruno.brolesi@mercadolivre.com>
2022-11-17 18:09:52 -0300

    feat: add compare hash and string in bcrypt service

--- internal/user/infra/service/bcrypt_hash_service.go
+++ internal/user/infra/service/bcrypt_hash_service.go
@@ -25,3 +25,9 @@ func (b bcryptHashService) GenerateHash(s string) (gateway.Hash, error) {
 
 	return string(hash), nil
 }
+
+func (bcryptHashService) CompareStringAndHash(s, hash string) error {
+	stringBytes := []byte(s)
+	hashBytes := []byte(hash)
+	return bcrypt.CompareHashAndPassword(hashBytes, stringBytes)
+}
