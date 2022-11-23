4a5c8c5bd1dcb9758545597f051adabfea32c5fa
Bruno Brolesi <bruno.brolesi@mercadolivre.com>
2022-11-23 18:30:26 -0300

    feat: add GetFarmsByUserId in farm repository

--- internal/farm/infra/repository/farm_repository.go
+++ internal/farm/infra/repository/farm_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"errors"
 
 	"github.com/brunobrolesi/open-garden-core/internal/farm/business/gateway"
 	"github.com/brunobrolesi/open-garden-core/internal/farm/business/model"
@@ -9,7 +10,8 @@ import (
 )
 
 const (
-	createFarmQuery = "insert into farms(name, address, owner, active) values($1, $2, $3, $4) returning (id)"
+	createFarmQuery      = "insert into farms(name, address, owner, active) values($1, $2, $3, $4) returning (id)"
+	getAllUserFarmsQuery = "SELECT id, name, address, owner, active FROM farms WHERE owner=$1"
 )
 
 type postgresFarmRepository struct {
@@ -25,4 +27,28 @@ func NewPostgresFarmRepository(conn *pgx.Conn) gateway.FarmRepository {
 func (r postgresFarmRepository) CreateFarm(farm model.Farm, ctx context.Context) (model.Farm, error) {
 	err := r.conn.QueryRow(ctx, createFarmQuery, farm.Name, farm.Address, farm.Owner, farm.Active).Scan(&farm.Id)
 	return farm, err
+}
+
+func (r postgresFarmRepository) GetFarmsByUserId(userId int, ctx context.Context) (model.Farms, error) {
+	rows, err := r.conn.Query(ctx, getAllUserFarmsQuery, userId)
+
+	if !errors.Is(err, pgx.ErrNoRows) && err != nil {
+		return model.Farms{}, err
+	}
+
+	defer rows.Close()
+
+	farms := model.Farms{}
+
+	for rows.Next() {
+		f := model.Farm{}
+		rows.Scan(&f.Id, &f.Name, &f.Address, &f.Owner, &f.Active)
+		farms = append(farms, f)
+	}
+
+	if err = rows.Err(); err != nil {
+		return model.Farms{}, err
+	}
+
+	return farms, nil
 }
