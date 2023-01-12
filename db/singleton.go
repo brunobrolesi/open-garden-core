f8e30634403a6018a58f1324de1014a9214aae8d
Bruno Brolesi <bruno.brolesi@mercadolivre.com>
2023-01-11 21:19:38 -0300

    refactor: improve singleton

--- db/singleton.go
+++ db/singleton.go
@@ -9,21 +9,17 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-var lock = &sync.Mutex{}
-
 var singleInstance *pgx.Conn
+var once = &sync.Once{}
 
 func GetInstance() *pgx.Conn {
-	if singleInstance == nil {
-		lock.Lock()
-		defer lock.Unlock()
-		if singleInstance == nil {
-			conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-			if err != nil {
-				log.Fatal("database connection fail")
-			}
-			singleInstance = conn
+	once.Do(func() {
+		conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+		if err != nil {
+			log.Fatal("database connection fail")
 		}
-	}
+		singleInstance = conn
+	})
+
 	return singleInstance
 }
