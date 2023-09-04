cb02419401b41c3f5ad3ff2c6fac92d8d0d695d0
Bruno Brolesi <brunohsbrolesi@gmail.com>
2023-09-03 22:06:28 -0300

    add measurements router and set trusted proxies

--- internal/api/router/router.go
+++ internal/api/router/router.go
@@ -4,6 +4,7 @@ import (
 	"github.com/brunobrolesi/open-garden-core/internal/api/middleware"
 	farmRouter "github.com/brunobrolesi/open-garden-core/internal/farm/infra/delivery/router"
 	healthRouter "github.com/brunobrolesi/open-garden-core/internal/health/infra/delivery/router"
+	measurementsRouter "github.com/brunobrolesi/open-garden-core/internal/measurements/infra/delivery/router"
 	sensorRouter "github.com/brunobrolesi/open-garden-core/internal/sensor/infra/delivery/router"
 	userRouter "github.com/brunobrolesi/open-garden-core/internal/user/infra/delivery/router"
 	"github.com/gin-gonic/gin"
@@ -11,6 +12,8 @@ import (
 
 func ApiRouter() *gin.Engine {
 	router := gin.Default()
+	router.ForwardedByClientIP = true
+	router.SetTrustedProxies([]string{"127.0.0.1"})
 
 	router.Group("/api")
 	{
@@ -18,6 +21,7 @@ func ApiRouter() *gin.Engine {
 		userRouter.UserRouter(router)
 		farmRouter.FarmRouter(router, middleware.AuthMiddlewareFactory())
 		sensorRouter.SensorRouter(router, middleware.AuthMiddlewareFactory())
+		measurementsRouter.MeasurementRouter(router, middleware.AuthMiddlewareFactory())
 	}
 
 	return router
